dd34691cc478d99bd5a9809c616c99cd5c7c6c86
ForGetASD <vitayz60@gmail.com>
2022-03-24 13:14:55 +0100

    globalis valtozo és valasz tomb

--- src/szokitalalo/SzoKitalalo.java
+++ src/szokitalalo/SzoKitalalo.java
@@ -2,7 +2,7 @@ package szokitalalo;
 public class SzoKitalalo {
 
     public static void main(String[] args) {
-        
+        matepro();
     }
     public static void szokitalal(){
     
@@ -14,14 +14,14 @@ public class SzoKitalalo {
     
     }
     public static String[] zalanpro(){
-        String valasz[] = new String[3];
+        String valasz[] = new String[]{"asd1", "asd2", "\u001B[41m"};
         return valasz;
     }
     public static void matepro(){
-    
+        String ansi_red = zalanpro()[2];
+        System.out.println(ansi_red + "szia");
     }
     public static void eredmeny(){
     
     }
-    
 }
